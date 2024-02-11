cb73773e6fec3dd18b1d67022c4200e19115e144
Manimohan R <manimohan.r@blockchainappfactory.com>
2024-02-11 17:44:27 +0530

    next.config changes 12

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
 
   //   // optimizeCss: true,
   // },
-  runtime: "experimental-edge",
   reactStrictMode: true,
   swcMinify: true,
   // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
