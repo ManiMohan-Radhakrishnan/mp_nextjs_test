b3b2865cff90520c60453637358d00d4e7fbd8c1
Manimohan R <manimohan.r@blockchainappfactory.com>
2024-02-11 17:02:03 +0530

    next.config changes 9

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextBuildId = require("next-build-id");
 const nextConfig = {
-  output: "export",
-
   // experimental: {
   //   // target: "serverless",
 
